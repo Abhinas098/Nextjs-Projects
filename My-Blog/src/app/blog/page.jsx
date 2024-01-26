59f9c0268727e5d8f29cc32bd690391df4fabad9
AbhinasDash <abhinasdash143@gmail.com>
2024-01-26 13:45:02 +0530

    responsive nav

--- My-Blog/src/app/blog/page.jsx
+++ My-Blog/src/app/blog/page.jsx
@@ -1,7 +1,86 @@
 import React from "react";
 
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div>
+      <p>
+        Blogafjsfj Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Deleniti autem veritatis illo blanditiis adipisci tempore est quisquam
+        id dignissimos? Doloremque quisquam beatae harum sunt totam dolore
+        perspiciatis quia dolor quaerat.lore Lorem ipsum, dolor sit amet
+        consectetur adipisicing elit. Aliquam qui eveniet asperiores cum nobis
+        aperiam voluptatum assumenda similique, ipsa ad voluptatem eum quos
+        vitae eaque. Dignissimos distinctio voluptatum in ex.lor
+      </p>
+      <br /> Blogafjsfj Lorem ipsum dolor, sit amet consectetur adipisicing
+      elit. Deleniti autem veritatis illo blanditiis adipisci tempore est
+      quisquam id dignissimos? Doloremque quisquam beatae harum sunt totam
+      dolore perspiciatis quia dolor quaerat.lore Lorem ipsum, dolor sit amet
+      consectetur adipisicing elit. Aliquam qui eveniet asperiores cum nobis
+      aperiam voluptatum assumenda similique, ipsa ad voluptatem eum quos vitae
+      eaque. Dignissimos distinctio voluptatum in ex.lor
+      <br /> Blogafjsfj Lorem ipsum dolor, sit amet consectetur adipisicing
+      elit. Deleniti autem veritatis illo blanditiis adipisci tempore est
+      quisquam id dignissimos? Doloremque quisquam beatae harum sunt totam
+      dolore perspiciatis quia dolor quaerat.lore Lorem ipsum, dolor sit amet
+      consectetur adipisicing elit. Aliquam qui eveniet asperiores cum nobis
+      aperiam voluptatum assumenda similique, ipsa ad voluptatem eum quos vitae
+      eaque. Dignissimos distinctio voluptatum in ex.lor
+      <br /> Blogafjsfj Lorem ipsum dolor, sit amet consectetur adipisicing
+      elit. Deleniti autem veritatis illo blanditiis adipisci tempore est
+      quisquam id dignissimos? Doloremque quisquam beatae harum sunt totam
+      dolore perspiciatis quia dolor quaerat.lore Lorem ipsum, dolor sit amet
+      consectetur adipisicing elit. Aliquam qui eveniet asperiores cum nobis
+      aperiam voluptatum assumenda similique, ipsa ad voluptatem eum quos vitae
+      eaque. Dignissimos distinctio voluptatum in ex.lor
+      <br /> Blogafjsfj Lorem ipsum dolor, sit amet consectetur adipisicing
+      elit. Deleniti autem veritatis illo blanditiis adipisci tempore est
+      quisquam id dignissimos? Doloremque quisquam beatae harum sunt totam
+      dolore perspiciatis quia dolor quaerat.lore Lorem ipsum, dolor sit amet
+      consectetur adipisicing elit. Aliquam qui eveniet asperiores cum nobis
+      aperiam voluptatum assumenda similique, ipsa ad voluptatem eum quos vitae
+      eaque. Dignissimos distinctio voluptatum in ex.lor
+      <div>
+        <p>
+          Blogafjsfj Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Deleniti autem veritatis illo blanditiis adipisci tempore est quisquam
+          id dignissimos? Doloremque quisquam beatae harum sunt totam dolore
+          perspiciatis quia dolor quaerat.lore Lorem ipsum, dolor sit amet
+          consectetur adipisicing elit. Aliquam qui eveniet asperiores cum nobis
+          aperiam voluptatum assumenda similique, ipsa ad voluptatem eum quos
+          vitae eaque. Dignissimos distinctio voluptatum in ex.lor
+        </p>
+        <br /> Blogafjsfj Lorem ipsum dolor, sit amet consectetur adipisicing
+        elit. Deleniti autem veritatis illo blanditiis adipisci tempore est
+        quisquam id dignissimos? Doloremque quisquam beatae harum sunt totam
+        dolore perspiciatis quia dolor quaerat.lore Lorem ipsum, dolor sit amet
+        consectetur adipisicing elit. Aliquam qui eveniet asperiores cum nobis
+        aperiam voluptatum assumenda similique, ipsa ad voluptatem eum quos
+        vitae eaque. Dignissimos distinctio voluptatum in ex.lor
+        <br /> Blogafjsfj Lorem ipsum dolor, sit amet consectetur adipisicing
+        elit. Deleniti autem veritatis illo blanditiis adipisci tempore est
+        quisquam id dignissimos? Doloremque quisquam beatae harum sunt totam
+        dolore perspiciatis quia dolor quaerat.lore Lorem ipsum, dolor sit amet
+        consectetur adipisicing elit. Aliquam qui eveniet asperiores cum nobis
+        aperiam voluptatum assumenda similique, ipsa ad voluptatem eum quos
+        vitae eaque. Dignissimos distinctio voluptatum in ex.lor
+        <br /> Blogafjsfj Lorem ipsum dolor, sit amet consectetur adipisicing
+        elit. Deleniti autem veritatis illo blanditiis adipisci tempore est
+        quisquam id dignissimos? Doloremque quisquam beatae harum sunt totam
+        dolore perspiciatis quia dolor quaerat.lore Lorem ipsum, dolor sit amet
+        consectetur adipisicing elit. Aliquam qui eveniet asperiores cum nobis
+        aperiam voluptatum assumenda similique, ipsa ad voluptatem eum quos
+        vitae eaque. Dignissimos distinctio voluptatum in ex.lor
+        <br /> Blogafjsfj Lorem ipsum dolor, sit amet consectetur adipisicing
+        elit. Deleniti autem veritatis illo blanditiis adipisci tempore est
+        quisquam id dignissimos? Doloremque quisquam beatae harum sunt totam
+        dolore perspiciatis quia dolor quaerat.lore Lorem ipsum, dolor sit amet
+        consectetur adipisicing elit. Aliquam qui eveniet asperiores cum nobis
+        aperiam voluptatum assumenda similique, ipsa ad voluptatem eum quos
+        vitae eaque. Dignissimos distinctio voluptatum in ex.lor
+      </div>
+    </div>
+  );
 };
 
-export default Blog
+export default Blog;
--- My-Blog/src/components/header/headerLinks/NavLinks.jsx
+++ My-Blog/src/components/header/headerLinks/NavLinks.jsx
@@ -1,7 +1,9 @@
-import React from "react";
-import Link from "../link/Links";
+"use client";
+import React, { useState } from "react";
 import styles from "./NavLinks.module.css";
 import Links from "../link/Links";
+import Menu from "@/components/Svgs/Menu";
+import Close from "@/components/Svgs/Close";
 
 const NavLinks = () => {
   const links = [
@@ -11,11 +13,26 @@ const NavLinks = () => {
     { name: "Contact", path: "/contact" },
   ];
 
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className={styles.links}>
-      {links.map((link) => (
-        <Links link={link} key={link.name} />
-      ))}
+    <div>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <Links link={link} key={link.name} />
+        ))}
+      </div>
+
+      <div className={styles.menu} onClick={() => setOpen((open) => !open)}>
+        {!open ? <Menu /> : <Close />}
+      </div>
+      {open && (
+        <div className={styles.responsiveLinks}>
+          {links.map((link) => (
+            <Links link={link} key={link.name} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
